130bad83243ae273458b5d8ab749139aeae2f608
Abhishek <abhishek.srivastava1@tothenew.com>
2020-05-17 13:33:55 +0530

    done

---
+++ Authentication/burger-builder/src/App.js
@@ -0,0 +1,85 @@
+import React ,{Component} from 'react';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+//import Checkout from './containers/Checkout/Checkout'
+import { Route ,Switch,withRouter,Redirect} from 'react-router-dom'
+// import Orders from '../src/containers/Orders/Orders'
+// import Auth from './containers/Auth/Auth'
+import Logout from './containers/Auth/Logout/Logout'
+import {connect} from 'react-redux'
+import * as actions from './store/actions/index'
+import asyncComponent from '../src/hoc/asyncComponent';
+
+const asyncCheckout=asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders=asyncComponent(()=>{
+  return import('../src/containers/Orders/Orders');
+});
+
+const asyncAuth=asyncComponent(()=>{
+  return import('./containers/Auth/Auth');
+});
+class App extends Component
+{
+  // state={
+  //   show:true
+  // }
+
+  // componentDidMount(){
+  //   setTimeout(()=>{
+  //     this.setState({show:false});
+  //   },5000)
+  // }
+
+  componentDidMount(){
+    this.props.onTryAutoSignup();
+  }
+  
+
+  render(){
+    let routes=(
+      <Switch>
+        <Route path="/auth" component={asyncAuth} />
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to='/'/>
+      </Switch>
+    );
+
+    if(this.props.isAuthenticated){
+      routes=(
+        <Switch>
+          <Route path="/orders" component={asyncOrders}/>
+          <Route path="/checkout" component={asyncCheckout}/>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/auth" component={asyncAuth} />
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to='/'/>
+        </Switch>
+      );
+    }
+
+    return(
+    <div>
+      <Layout>
+        {/* {this.state.show?<BurgerBuilder/>:null} */}
+        {routes}
+      </Layout>
+    </div>
+    );
+  }
+}
+
+const mapStateToProps=state=>{
+  return{
+    isAuthenticated:state.auth.token!==null
+
+  }
+}
+const mapDispatchToProps=dispatch=>{
+  return{
+    onTryAutoSignup:()=>dispatch(actions.authCheckState())
+  }
+}
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
---
+++ Authentication/burger-builder/src/containers/Orders/Orders.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import Order from '../../components/Order/Order'
+import axios from '../../axios-order'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../store/actions/index'
+import {connect} from 'react-redux'
+import Spinner from '../../components/UI/Spinner/Spinner'
+
+class Orders extends Component {
+
+    componentDidMount(){
+        console.log(this.props.token);
+        this.props.onFetchOrders(this.props.token,this.props.userId);
+
+        //***********WITHOUT REDUX*********** */
+        // axios.get('/orders.json')
+        // .then(response=>{
+        //     const fetchedOrders=[];
+        //     for(let key in response.data){
+        //         fetchedOrders.push({
+        //             ...response.data[key],
+        //         id:key
+        //     });
+        //     }
+        //     this.setState({loading:false,orders:fetchedOrders})}
+        //     ).catch(error=>{
+        //         this.setState({loading:false})
+        //     })
+    }
+    render() {
+        let orders=<Spinner/>
+        if(!this.props.loading){
+            orders=(
+                this.props.orders.map(order=>(
+                    <Order key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}
+                    />
+                ))
+            )
+        }
+        return (
+            <div>
+                {orders}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps=state=>{
+    return{
+        orders:state.order.orders,
+        loading:state.order.loading,
+        token:state.auth.token,
+        userId:state.auth.userId
+    }
+};
+
+const mapDispatchToProps= dispatch => {
+    return{
+        onFetchOrders:(token,userId)=>dispatch(actions.fetchOrders(token,userId))
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(Orders,axios));
